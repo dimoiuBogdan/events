786fbaef5226f4c82a97e1b046b0e0068ad30332
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2023-12-27 01:19:01 +0200

    Append

--- src/common/components/NewVersion/NewVersion.ts
+++ src/common/components/NewVersion/NewVersion.ts
@@ -2,10 +2,12 @@ const sw = self;
 
 // @ts-expect-error will work on later
 sw.register({
+  // @ts-expect-error will work on later
   onUpdate: (registration) => {
     const waitingServiceWorker = registration.waiting;
 
     if (waitingServiceWorker) {
+      // @ts-expect-error will work on later
       waitingServiceWorker.addEventListener("statechange", (event) => {
         if (event.target.state === "activated") {
           if (
