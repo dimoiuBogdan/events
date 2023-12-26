a3db176617b464ed7bfaf4d4909cf79a619b9dce
Dimoiu Bogdan <bogdan.dimoiu@itperspectives.ro>
2023-12-27 01:25:43 +0200

    Moved sw

--- src/main.tsx
+++ src/main.tsx
@@ -16,5 +16,31 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </PrimeReactProvider>
     </ReduxProvider>
-  </StrictMode>
+  </StrictMode>,
 );
+
+const sw = self;
+
+// @ts-expect-error will work on later
+sw.register({
+  // @ts-expect-error will work on later
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      // @ts-expect-error will work on later
+      waitingServiceWorker.addEventListener("statechange", (event) => {
+        if (event.target.state === "activated") {
+          if (
+            window.confirm(
+              "There is a new version of the app ready. Please reload to update.",
+            )
+          ) {
+            window.location.reload();
+          }
+        }
+      });
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+    }
+  },
+});
